d5aa93c1d224a4ea65d7f607d1b4be6df2f59456
Kedaya <thss1961@163.com>
2024-01-12 00:47:43 +0800

    需求/描述: 1、初始化 项目: main 提交人: Kedaya 时间: 2024-01-12 00:47:03

---
+++ src/main/java/com/tth/service/JetcacheServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetcacheServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetcacheServiceApplication.class, args);
+    }
+
+}
